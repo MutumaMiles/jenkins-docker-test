f73b09f4c8c872ac9fae274334f63ef3199290f2
Mutuma Dev <mutumamiles@gmail.com>
2020-08-12 21:01:08 +0300

    first commit

---
+++ src/main/java/com/example/jenkinsdockertest/JenkinsDockerTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.jenkinsdockertest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsDockerTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JenkinsDockerTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/jenkinsdockertest/JenkinsDockerTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jenkinsdockertest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsDockerTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
